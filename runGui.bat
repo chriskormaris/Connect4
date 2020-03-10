9157e34be66618f86daf9ada55d6fa9c466b036d
Christos Kormaris <chriskormaris@gmail.com>
2020-03-10 12:25:07 +0200

    Update runGui.bat
    Minor changes. Remove unneeded "\", "." and ";" characters.

--- runGui.bat
+++ runGui.bat
@@ -1,6 +1,6 @@
 :: This file is for Windows only.
 
 :: copy the "images" folder from "res\images" inside the "bin" directory
-cp res\images bin\
-java -cp .\bin gui.Gui
-pause;
+cp res\images bin
+java -cp bin gui.Gui
+pause
