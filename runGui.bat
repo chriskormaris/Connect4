9d12a65ea8841307e112a2498ab8979adad35108
Iptamenos <chriskormaris@gmail.com>
2020-03-27 22:13:16 +0200

    Rename the resource images, to match the code.

--- runGui.bat
+++ runGui.bat
@@ -2,6 +2,6 @@
 
 :: copy the "images" folder from "res\images" inside the "bin" directory
 if not exist bin\images mkdir bin\images
-cp res\images\* bin\images
+copy res\images\* bin\images
 java -cp bin gui.Gui
 pause
