ba5ac56726bef4d34f7b63138eb21bc4feddf094
Christos Kormaris <chriskormaris@gmail.com>
2020-06-22 23:13:16 +0300

    Java ".bat" file changes.

--- runGui.bat
+++ runGui.bat
@@ -1,7 +1,4 @@
 :: This file is for Windows only.
 
-:: copy the "images" folder from "res\images" inside the "bin" directory
-if not exist bin\images mkdir bin\images
-copy res\images\* bin\images
 java -cp bin gui.Gui
 pause
