b2e69af1fbafac2a125f506652f049d0b511c7ed
Iptamenos <chriskormaris@gmail.com>
2020-06-22 23:13:16 +0300

    Java ".bat" file changes.

--- compileAll.bat
+++ compileAll.bat
@@ -2,4 +2,9 @@
 
 if not exist bin mkdir bin
 javac src\connect4\*.java src\gui\*.java -d bin
+
+:: Copy the folder "res\images" to "bin".
+if not exist bin\images mkdir bin\images
+copy res\images bin\images
+
 pause
