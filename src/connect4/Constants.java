037bc45a311a13c1ddb39fc8e6a5efabe1a4618f
Phgh21096 <74558199+Phgh21096@users.noreply.github.com>
2021-01-23 22:06:04 +0200

    Update Constants.java

--- src/connect4/Constants.java
+++ src/connect4/Constants.java
@@ -3,7 +3,7 @@ package connect4;
 
 public class Constants {
 	
-	private Constants() { }  // Prevents instantiation.
+	private Constants() { } 
 	
 	public static final double VERSION = 2.5;
 	
@@ -12,8 +12,8 @@ public class Constants {
 	public static final int IN_A_ROW = 4;
 	
 	// Board values
-	public static final int P1 = 1;  // Player 1
-	public static final int P2 = 2;  // Player 2
+	public static final int P1 = 1;  
+	public static final int P2 = 2;  
 	public static final int EMPTY = 0;
 	
 }
