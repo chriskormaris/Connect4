edc0b23e4f4e28cbe6dbeac7023480e3ee3b7414
Iptamenos <chriskormaris@gmail.com>
2021-07-20 00:19:31 +0300

    Update `.gitignore`
    Untrack `Connect4.jar`.
    Change VERSION from "2.5" to "2.5.0".

--- src/utility/Constants.java
+++ src/utility/Constants.java
@@ -5,7 +5,7 @@ public class Constants {
 	
 	private Constants() { }  // Prevents instantiation.
 	
-	public static final double VERSION = 2.5;
+	public static final String VERSION = "2.5.0";
 	
 	public static final int NUM_OF_ROWS = 6;
 	public static final int NUM_OF_COLUMNS = 7;
