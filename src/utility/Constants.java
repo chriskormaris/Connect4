f7fd5e421a6bc8fb97cf9a0b48737c59347d41a0
Christos Kormaris <chriskormaris@gmail.com>
2021-09-12 01:18:27 +0300

    Bump up version to 2.5.2

--- src/utility/Constants.java
+++ src/utility/Constants.java
@@ -5,7 +5,7 @@ public class Constants {
 	
 	private Constants() { }  // Prevents instantiation.
 	
-	public static final String VERSION = "2.5.1";
+	public static final String VERSION = "2.5.2";
 
 	public static final int AI_MOVE_MILLISECONDS = 250;
 	
