ad4ec62b30ab84a6ea64323cb11a08bebfc8373c
Christos Kormaris <chriskormaris@gmail.com>
2022-03-05 20:24:28 +0200

    Update `compileAll.bat` and `runGui.bat`

--- compileAll.bat
+++ compileAll.bat
@@ -1,7 +1,7 @@
 :: This file is for Windows only.
 
 if not exist bin mkdir bin
-javac src\connect4\*.java src\gui\*.java -d bin
+javac src\com\chriskormaris\connect4\api\ai\*.java src\com\chriskormaris\connect4\api\connect4\*.java src\com\chriskormaris\connect4\api\enumeration\*.java src\com\chriskormaris\connect4\api\util\*.java src\com\chriskormaris\connect4\console\*.java src\com\chriskormaris\connect4\gui\*.java src\com\chriskormaris\connect4\gui\enumeration\*.java src\com\chriskormaris\connect4\gui\util\*.java -d bin
 
 :: Copy the folder "res\images" to "bin".
 if not exist bin\images mkdir bin\images
--- runGui.bat
+++ runGui.bat
@@ -1,4 +1,4 @@
 :: This file is for Windows only.
 
-java -cp bin gui.Gui
+java -cp bin com.chriskormaris.connect4.gui.GUI
 pause
