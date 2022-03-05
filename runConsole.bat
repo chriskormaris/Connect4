b2dd814a5af065e4e2d5592805b74914fbd7a79f
Christos Kormaris <chriskormaris@gmail.com>
2022-03-05 20:28:21 +0200

    Split `runConsole.bat` into `runConsoleConnect4.bat` and `runConsoleConnect5.bat`
    Also update `.gitignore`.

--- runConsole.bat
+++
@@ -1,4 +0,0 @@
-:: This file is for Windows only.
-
-java -cp bin connect4.ConsoleMain
-pause
---
+++ runConsoleConnect5.bat
@@ -0,0 +1,4 @@
+:: This file is for Windows only.
+
+java -cp bin com.chriskormaris.connect4.console.Connect5
+pause
