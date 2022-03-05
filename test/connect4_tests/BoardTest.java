e0bfa4a85ac30762b37fce84ece65d0aa70a93ac
Christos Kormaris <chriskormaris@gmail.com>
2022-03-05 15:47:30 +0200

    Use JUnit 4 instead of Jupiter test library

--- test/connect4_tests/BoardTest.java
+++ test/connect4_tests/BoardTest.java
@@ -4,10 +4,11 @@ package connect4_tests;
 import com.chriskormaris.connect4.api.connect4.Board;
 import com.chriskormaris.connect4.api.util.Constants;
 import com.chriskormaris.connect4.gui.GUI;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.Before;
+import org.junit.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.Assert.assertEquals;
+// import static org.junit.Assert.fail;
 
 
 public class BoardTest {
@@ -15,56 +16,58 @@ public class BoardTest {
     static final int numOfRows = GUI.gameParameters.getNumOfRows();
     static final int numOfColumns = GUI.gameParameters.getNumOfColumns();
 
-    @BeforeEach
+    @Before
     public void setUp() {
 
     }
 
-//	@Test
-//	public void testBoard() {
-//		fail("Not yet implemented");
-//	}
-//
-//
-//	@Test
-//	public void testGetLastMove() {
-//		fail("Not yet implemented");
-//	}
-//
-//	@Test
-//	public void testGetLastLetterPlayed() {
-//		fail("Not yet implemented");
-//	}
-//
-//	@Test
-//	public void testGetGameBoard() {
-//		fail("Not yet implemented");
-//	}
-//
-//	@Test
-//	public void testGetWinner() {
-//		fail("Not yet implemented");
-//	}
-//
-//	@Test
-//	public void testSetLastMove() {
-//		fail("Not yet implemented");
-//	}
-//
-//	@Test
-//	public void testSetLastLetterPlayed() {
-//		fail("Not yet implemented");
-//	}
-//
-//	@Test
-//	public void testSetGameBoard() {
-//		fail("Not yet implemented");
-//	}
-//
-//	@Test
-//	public void testSetWinner() {
-//		fail("Not yet implemented");
-//	}
+    /*
+	@Test
+	public void testBoard() {
+		fail("Not yet implemented");
+	}
+
+
+	@Test
+	public void testGetLastMove() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetLastLetterPlayed() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetGameBoard() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetWinner() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetLastMove() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetLastLetterPlayed() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetGameBoard() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetWinner() {
+		fail("Not yet implemented");
+	}
+	*/
 
     @Test
     public void testMakeMoveInt() {
@@ -73,31 +76,33 @@ public class BoardTest {
         b.makeMove(0, Constants.P1);
         Board.printBoard(b.getGameBoard());
 
-        assertEquals(Constants.P1, b.getGameBoard()[5][0], "The board was not updated correctly.");
-        assertEquals(Constants.P1, b.getLastPlayer(), "The last player symbol played is not correct.");
+        assertEquals("The board was not updated correctly.", Constants.P1, b.getGameBoard()[5][0]);
+        assertEquals("The last player symbol played is not correct.", Constants.P1, b.getLastPlayer());
         System.out.println("*****************************");
         System.out.println();
     }
 
-//	@Test
-//	public void testIsValidMoveIntInt() {
-//		fail("Not yet implemented");
-//	}
-//
-//	@Test
-//	public void testIsValidMoveInt() {
-//		fail("Not yet implemented");
-//	}
-//
-//	@Test
-//	public void testCanMove() {
-//		fail("Not yet implemented");
-//	}
-//
-//	@Test
-//	public void testCheckFullColumn() {
-//		fail("Not yet implemented");
-//	}
+    /*
+	@Test
+	public void testIsValidMoveIntInt() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testIsValidMoveInt() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCanMove() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCheckFullColumn() {
+		fail("Not yet implemented");
+	}
+	*/
 
     @Test
     public void testGetRowPosition() {
@@ -112,45 +117,47 @@ public class BoardTest {
 
         for (int col = 0; col < numOfColumns; col++) {
             int row = b.getEmptyRowPosition(col);
-            assertEquals(4, row, "The row result is not the expected one.");
+            assertEquals("The row result is not the expected one.", 4, row);
         }
         System.out.println("*****************************");
         System.out.println();
     }
 
-//	@Test
-//	public void testGetChildren() {
-//		fail("Not yet implemented");
-//	}
-//
-//	@Test
-//	public void testEvaluate() {
-//		fail("Not yet implemented");
-//	}
-//
-//	@Test
-//	public void testCheckWinState() {
-//		fail("Not yet implemented");
-//	}
-//
-//	@Test
-//	public void testCheck3InARow() {
-//		fail("Not yet implemented");
-//	}
-//
-//	@Test
-//	public void testCheck2InARow() {
-//		fail("Not yet implemented");
-//	}
-//
-//	@Test
-//	public void testIsTerminal() {
-//		fail("Not yet implemented");
-//	}
-//
-//	@Test
-//	public void testPrint() {
-//		fail("Not yet implemented");
-//	}
+    /*
+	@Test
+	public void testGetChildren() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testEvaluate() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCheckWinState() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCheck3InARow() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCheck2InARow() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testIsTerminal() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testPrint() {
+		fail("Not yet implemented");
+	}
+	*/
 
 }
