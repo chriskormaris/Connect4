a02d2a6a48dc0105f09cb3f66f27dc73d20d9774
Christos Kormaris <chriskormaris@gmail.com>
2022-03-13 01:45:48 +0200

    Fix bugs in MinimaxAI

--- src/com/chriskormaris/connect4/api/ai/MinimaxAI.java
+++ src/com/chriskormaris/connect4/api/ai/MinimaxAI.java
@@ -55,7 +55,7 @@ public class MinimaxAI extends AI {
             return new Move(board.getLastMove().getRow(), board.getLastMove().getColumn(), board.evaluate());
         }
         // The children-moves of the state are calculated
-        ArrayList<Board> children = new ArrayList<Board>(board.getChildren(Constants.P1));
+        ArrayList<Board> children = new ArrayList<>(board.getChildren(Constants.P1));
         Move maxMove = new Move(Integer.MIN_VALUE);
         for (Board child : children) {
             // And for each child min is called, on a lower depth
@@ -64,7 +64,7 @@ public class MinimaxAI extends AI {
             if (move.getValue() >= maxMove.getValue()) {
                 if ((move.getValue() == maxMove.getValue())) {
                     // If the heuristic has the same value then we randomly choose one of the two moves
-                    if (r.nextInt(2) == 0) {
+                    if (r.nextInt(2) == 0 || move.getValue() == Integer.MIN_VALUE) {
                         maxMove.setRow(child.getLastMove().getRow());
                         maxMove.setColumn(child.getLastMove().getColumn());
                         maxMove.setValue(move.getValue());
@@ -86,13 +86,13 @@ public class MinimaxAI extends AI {
         if ((board.checkForGameOver()) || (depth == maxDepth)) {
             return new Move(board.getLastMove().getRow(), board.getLastMove().getColumn(), board.evaluate());
         }
-        ArrayList<Board> children = new ArrayList<Board>(board.getChildren(Constants.P2));
+        ArrayList<Board> children = new ArrayList<>(board.getChildren(Constants.P2));
         Move minMove = new Move(Integer.MAX_VALUE);
         for (Board child : children) {
             Move move = max(child, depth + 1);
             if (move.getValue() <= minMove.getValue()) {
                 if ((move.getValue() == minMove.getValue())) {
-                    if (r.nextInt(2) == 0) {
+                    if (r.nextInt(2) == 0 || move.getValue() == Integer.MAX_VALUE) {
                         minMove.setRow(child.getLastMove().getRow());
                         minMove.setColumn(child.getLastMove().getColumn());
                         minMove.setValue(move.getValue());
