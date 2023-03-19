0e9f86da748c4316f915b8243df5a521cd035273
Christos Kormaris <chriskormaris@gmail.com>
2023-03-19 23:21:47 +0200

    Version 2.8.3: Increase the height of Connect-4 and Connect-5 GUI to fit the board image

--- gui/src/main/java/com/chriskormaris/connect4/gui/util/GuiConstants.java
+++ gui/src/main/java/com/chriskormaris/connect4/gui/util/GuiConstants.java
@@ -10,15 +10,15 @@ public class GuiConstants {
 	public static final String CONNECT_4_TITLE = "Connect-4";
 	public static final String CONNECT_5_TITLE = "Connect-5";
 
-	public static final String VERSION = "2.8.2";
+	public static final String VERSION = "2.8.3";
 
 	public static final String CONNECT_4_BOARD_IMG_PATH = "images/Board4.png";
 	public static final String CONNECT_5_BOARD_IMG_PATH = "images/Board5.png";
 
 	public static final int DEFAULT_CONNECT_4_WIDTH = 600;
-	public static final int DEFAULT_CONNECT_4_HEIGHT = 625;
+	public static final int DEFAULT_CONNECT_4_HEIGHT = 635;
 
 	public static final int DEFAULT_CONNECT_5_WIDTH = 670;
-	public static final int DEFAULT_CONNECT_5_HEIGHT = 700;
+	public static final int DEFAULT_CONNECT_5_HEIGHT = 710;
 
 }
