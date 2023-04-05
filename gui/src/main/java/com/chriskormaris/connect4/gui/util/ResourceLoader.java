71eead0207652039ec84931b833780dce1b191f7
Christos Kormaris <chriskormaris@gmail.com>
2023-04-05 21:43:45 +0300

    Code maintenance

--- gui/src/main/java/com/chriskormaris/connect4/gui/util/ResourceLoader.java
+++ gui/src/main/java/com/chriskormaris/connect4/gui/util/ResourceLoader.java
@@ -9,13 +9,10 @@ import java.net.URL;
 public final class ResourceLoader {
 
 	public static URL load(String path) {
-		// InputStream input = ResourceLoader.class.getResourceAsStream(path);
 		URL input = ResourceLoader.class.getResource(path);
 		if (input == null) {
 			input = ResourceLoader.class.getResource("/" + path);
 		}
-		// String in = input.getPath();
-		// System.out.println("in: " + in);
 		return input;
 	}
 
